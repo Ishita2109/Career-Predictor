cac390599e48b5749a9cacfc51ed3eb16f06be10
HomePage <knowtogrow333@gmail.com>
2021-05-23 14:00:23 +0530

    Hello

---
+++ test/src/main/java/com/example/test/Test.java
@@ -0,0 +1,25 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.navigationtry.Home;
+import com.example.navigationtry.LoadPage;
+import com.example.predictor.Predict;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+    public void btn_test(View view) {
+        Intent i= new Intent(this, Predict.class);
+        startActivity(i);
+    }
+}
